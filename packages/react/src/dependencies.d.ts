d38fbc270facf75f1400b239858f29f7c8f660e4
Moroine Bentefrit <moroine.bentefrit@gmail.com>
2020-11-02 11:09:24 +0100

    feat: add Participant, Publication & Track compatible with Twilio

--- packages/react/src/dependencies.d.ts
+++ packages/react/src/dependencies.d.ts
@@ -8,3 +8,13 @@ declare module "*.module.css" {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare module "*.mp4" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.mp3" {
+  const src: string;
+  export default src;
+}
---
+++ packages/react/src/entities/fixtures/participants.fixture.ts
@@ -0,0 +1,1 @@
+import { RawParticipant } from "../Participant";
