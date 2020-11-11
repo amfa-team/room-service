964258dc1ad337e74290e59d40475d5abeea1b89
Moroine Bentefrit <moroine.bentefrit@gmail.com>
2020-11-11 21:53:49 +0100

    feat(api): add cron logs

--- packages/api/src/cron/cronHandler.ts
+++ packages/api/src/cron/cronHandler.ts
@@ -1,14 +1,20 @@
 import { ParticipantStatus } from "@amfa-team/types";
+import { logger } from "../io/logger";
 import { ParticipantModel } from "../mongo/model/participant";
 import { disconnectParticipant } from "../service/lifecycleService";
 
 export async function cronController() {
+  logger.info("cronController: start");
   const participants = await ParticipantModel.find({
     status: ParticipantStatus.pending,
     statusValidUntil: { $lt: new Date() },
   });
 
+  logger.info("cronController: will update", { participants });
+
   await Promise.all([
     participants.map(async (p) => disconnectParticipant(p, true)),
   ]);
+
+  logger.info("cronController: will did", { participants });
 }
