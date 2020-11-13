f53c6074a3d074f44aa7e7e30c4513400de5a13b
Gethi <bigard.antoine@gmail.com>
2020-11-13 15:14:47 +0100

    chore: styling participantList

--- packages/react/src/components/ParticipantList/ParticipantList.tsx
+++ packages/react/src/components/ParticipantList/ParticipantList.tsx
@@ -1,11 +1,37 @@
-import classnames from "classnames";
+import { motion, useAnimation } from "framer-motion";
 import React from "react";
 import type { IRoom } from "../../entities/Room";
 import useParticipants from "../../hooks/useParticipants";
-import useScreenShareParticipant from "../../hooks/useScreenShareParticipant";
 import { useSelectedParticipant } from "../../hooks/useSelectedParticipant";
 import Participant from "../Participant/Participant";
 import styles from "./participantList.module.css";
+
+const containerAnimation = {
+  hidden: {
+    opacity: 1,
+    scale: 0,
+  },
+  visible: {
+    scale: 1,
+    borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+    transition: {
+      delay: 0.1,
+      when: "beforeChildren",
+      staggerChildren: 0.05,
+    },
+  },
+};
+
+const itemAnimation = {
+  hidden: {
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+  },
+};
 
 export interface ParticipantListProps {
   room: IRoom;
@@ -24,40 +50,68 @@ export default function ParticipantList(
     selectedParticipant,
     setSelectedParticipant,
   ] = useSelectedParticipant();
-  const screenShareParticipant = useScreenShareParticipant(room);
-  const isRemoteParticipantScreenSharing =
-    screenShareParticipant && screenShareParticipant !== localParticipant;
+
+  const controls = useAnimation();
+  const controlsChildren = useAnimation();
+
+  const onShuffleClicked = async () => {
+    props.onShuffle();
+    const animationStart = controlsChildren.start("hidden");
+    await controls.start("visible");
+    await controlsChildren.start("visible");
+    return animationStart;
+  };
 
   return (
-    <aside
-      className={classnames(styles.container, {
-        [styles.transparentBackground]: !isRemoteParticipantScreenSharing,
-      })}
+    <motion.div
+      className={styles.container}
+      variants={containerAnimation}
+      initial="visible"
+      animate={controls}
     >
-      <div className={styles.scrollContainer}>
-        <button type="button" onClick={props.onShuffle}>
-          Shuffle
-        </button>
-        <Participant
-          participant={localParticipant}
-          isLocalParticipant
-          onClick={() => {
-            setSelectedParticipant(localParticipant);
-          }}
-        />
-        {participants.map((participant) => {
-          return (
+      <motion.div
+        className={styles.shuffleCTA}
+        whileTap={{ scale: 0.8 }}
+        onClick={async () => onShuffleClicked()}
+      >
+        <span>Shuffle</span>
+      </motion.div>
+      <ul className={styles.itemsContainer}>
+        <motion.li
+          className={styles.item}
+          variants={itemAnimation}
+          animate={controlsChildren}
+        >
+          <Participant
+            participant={localParticipant}
+            isLocalParticipant
+            onClick={() => {
+              setSelectedParticipant(localParticipant);
+            }}
+          />
+        </motion.li>
+        {participants.map((participant) => (
+          <motion.li
+            key={participant.sid}
+            className={styles.item}
+            variants={itemAnimation}
+            animate={controlsChildren}
+          >
             <Participant
-              key={participant.sid}
               participant={participant}
               isSelected={participant === selectedParticipant}
               onClick={() => {
                 setSelectedParticipant(participant);
               }}
             />
-          );
-        })}
-      </div>
-    </aside>
+          </motion.li>
+        ))}
+        <motion.li
+          className={styles.item}
+          variants={itemAnimation}
+          animate={controlsChildren}
+        />
+      </ul>
+    </motion.div>
   );
 }
