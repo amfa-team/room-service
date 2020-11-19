13f65765028dc7d04f2370e4d1e7f37dd3a5320c
Moroine Bentefrit <moroine.bentefrit@gmail.com>
2020-11-19 23:46:13 +0100

    chore: use sidebyside domain name

--- packages/react/release.config.js
+++ packages/react/release.config.js
@@ -7,8 +7,36 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
     "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+        message:
+          // eslint-disable-next-line no-template-curly-in-string
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
     "@semantic-release/github",
+    [
+      "@semantic-release/exec",
+      {
+        publishCmd: "git checkout -- package.json",
+      },
+    ],
+    [
+      "@saithodev/semantic-release-backmerge",
+      {
+        branchName: "develop",
+        plugins: [],
+      },
+    ],
     [
       "semantic-release-slack-bot",
       {
