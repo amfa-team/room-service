9a6eaa1d3a6420ca505399b8b8a12992561275ea
Moroine Bentefrit <moroine.bentefrit@gmail.com>
2020-11-26 23:11:49 +0100

    feat: ensure user is not banned

--- packages/api/src/io/types.ts
+++ packages/api/src/io/types.ts
@@ -1,4 +1,5 @@
 import type { AdminData, GetRoutes, PostRoutes } from "@amfa-team/types";
+import type { APIGatewayEventRequestContext } from "aws-lambda";
 
 export interface PublicRequest<T> {
   data: T;
@@ -14,9 +15,11 @@ export interface RequestContext {
 export type GetHandler<P extends keyof GetRoutes> = (
   params: Record<string, string> | null,
   headers: Record<string, string>,
+  requestContext: APIGatewayEventRequestContext,
 ) => Promise<GetRoutes[P]["out"]>;
 
 export type PostHandler<P extends keyof PostRoutes> = (
   data: PostRoutes[P]["in"],
   headers: Record<string, string>,
+  requestContext: APIGatewayEventRequestContext,
 ) => Promise<PostRoutes[P]["out"]>;
