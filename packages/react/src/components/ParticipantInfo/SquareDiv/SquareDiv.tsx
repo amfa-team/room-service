b384597164264dd3361ebc1a89270f83a39ebbf8
Moroine Bentefrit <moroine.bentefrit@gmail.com>
2021-02-05 18:18:17 +0100

    fix: add missing ResizeObserver polyfill

--- packages/react/src/components/ParticipantInfo/SquareDiv/SquareDiv.tsx
+++ packages/react/src/components/ParticipantInfo/SquareDiv/SquareDiv.tsx
@@ -1,22 +1,13 @@
 import classnames from "classnames";
 import { motion } from "framer-motion";
 import type { ReactNode } from "react";
-import React from "react";
-import ReactResizeDetector from "react-resize-detector";
+import React, { useMemo } from "react";
+import { useResizeDetector } from "react-resize-detector/build/withPolyfill";
 import classes from "./squareDiv.module.css";
 
 interface SquareDivProps {
   hidden: boolean;
   children: JSX.Element | JSX.Element[] | ReactNode;
-}
-
-interface RenderProps {
-  // eslint-disable-next-line react/no-unused-prop-types
-  height: number;
-  // eslint-disable-next-line react/no-unused-prop-types
-  width: number;
-  // eslint-disable-next-line react/no-unused-prop-types
-  targetRef: React.Ref<HTMLDivElement>;
 }
 
 const initial = { scale: 1.5, opacity: 0 };
@@ -29,37 +20,37 @@ const transition = {
 };
 
 export function SquareDiv(props: SquareDivProps) {
+  const { hidden, children } = props;
+  const { width, height, ref: targetRef } = useResizeDetector({
+    handleWidth: true,
+    handleHeight: true,
+    refreshMode: "debounce",
+    refreshOptions: { leading: true, trailing: true },
+    refreshRate: 50,
+  });
+
+  const size = Math.min(
+    Number.isFinite(height) ? height : 0,
+    Number.isFinite(width) ? width : 0,
+  );
+  const style = useMemo(() => {
+    return size === 0 ? {} : { height: size, width: size };
+  }, [size]);
+
   return (
-    <ReactResizeDetector
-      handleWidth
-      handleHeight
-      refreshMode="debounce"
-      refreshOptions={{ leading: true, trailing: true }}
-      refreshRate={50}
+    <motion.div
+      initial={initial}
+      animate={animate}
+      transition={transition}
+      className={classnames(classes.root, {
+        [classes.hidden]: hidden,
+      })}
+      ref={targetRef}
     >
-      {({ width, height, targetRef }: RenderProps) => {
-        const size = Math.min(
-          Number.isFinite(height) ? height : 0,
-          Number.isFinite(width) ? width : 0,
-        );
-        const style = size === 0 ? {} : { height: size, width: size };
-        return (
-          <motion.div
-            initial={initial}
-            animate={animate}
-            transition={transition}
-            className={classnames(classes.root, {
-              [classes.hidden]: props.hidden,
-            })}
-            ref={targetRef}
-          >
-            <div className={classes.content} style={style}>
-              {props.children}
-            </div>
-          </motion.div>
-        );
-      }}
-    </ReactResizeDetector>
+      <div className={classes.content} style={style}>
+        {children}
+      </div>
+    </motion.div>
   );
 }
 SquareDiv.defaultProps = {
